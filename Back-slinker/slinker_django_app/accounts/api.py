156f884ec36be52f4d466ad406d788f1c7559a38
AyoubTougui <mr.tougui.ayoub@gmail.com>
2021-07-17 18:44:09 +0100

    get user & logout

--- Back-slinker/slinker_django_app/accounts/api.py
+++ Back-slinker/slinker_django_app/accounts/api.py
@@ -32,3 +32,12 @@ class LoginAPI(generics.GenericAPIView):
 
 
 # get user api
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
--- Back-slinker/slinker_django_app/accounts/urls.py
+++ Back-slinker/slinker_django_app/accounts/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path,include
-from .api import RegisteAPI,LoginAPI
+from .api import RegisteAPI,LoginAPI,UserAPI
 from knox import views as knox_views
 
 urlpatterns=[
-    path('api/auth',include('knox.urls')),
+    # post request
+    # register url
     path('api/auth/register',RegisteAPI.as_view()),
-    path('api/auth/login',LoginAPI.as_view()),
+    # post request
+    # login url
+    path('api/auth/login',LoginAPI.as_view()), 
+    # get request
+    # get current user, pass token in headers
+    path('api/auth/user',UserAPI.as_view()), 
+    # post request
+    # logout url, pass token in headers
+    path('api/auth/logout',knox_views.LogoutView.as_view()), 
 ]

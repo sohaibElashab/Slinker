7765e80cd2b7926604a7dd05017be25b15226666
AyoubTougui <mr.tougui.ayoub@gmail.com>
2021-08-15 18:41:49 +0100

    api fix

--- Back-slinker/slinker_django_app/scrap/api.py
+++ Back-slinker/slinker_django_app/scrap/api.py
@@ -1,6 +1,9 @@
+from rest_framework.serializers import Serializer
 from scrap.models import Websites, Similar_products, Pages, Products
 from rest_framework import viewsets, permissions
 from .serializers import WebsitesSerializer, PagesSerializer, ProductsSerializer, SimilarProductsSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class WebsitesVS(viewsets.ModelViewSet):
@@ -8,11 +11,27 @@ class WebsitesVS(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = WebsitesSerializer
 
+    @action(detail=False, methods=['POST'])
+    def ID(self, request, pK=None):
+        web = Websites.objects.filter(user_id=request.data['user_id'])
+        a = []
+        for x in web:
+            a.append(WebsitesSerializer(x).data)
+        return Response(a)
+
 
 class PagesVS(viewsets.ModelViewSet):
     queryset = Pages.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = PagesSerializer
+
+    @action(detail=False, methods=['POST'])
+    def ID(self, request, pK=None):
+        web = Pages.objects.filter(website_id=request.data['website_id'])
+        a = []
+        for x in web:
+            a.append(PagesSerializer(x).data)
+        return Response(a)
 
 
 class ProductsVS(viewsets.ModelViewSet):
@@ -20,8 +39,25 @@ class ProductsVS(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = ProductsSerializer
 
+    @action(detail=False, methods=['POST'])
+    def ID(self, request, pK=None):
+        web = Products.objects.filter(website_id=request.data['website_id'])
+        a = []
+        for x in web:
+            a.append(ProductsSerializer(x).data)
+        return Response(a)
+
 
 class SimilarProductsVS(viewsets.ModelViewSet):
     queryset = Similar_products.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = SimilarProductsSerializer
+
+    @action(detail=False, methods=['POST'])
+    def ID(self, request, pK=None):
+        web = Similar_products.objects.filter(
+            product_id=request.data['product_id'])
+        a = []
+        for x in web:
+            a.append(SimilarProductsSerializer(x).data)
+        return Response(a)
--- Back-slinker/slinker_django_app/scrap/urls.py
+++ Back-slinker/slinker_django_app/scrap/urls.py
@@ -8,10 +8,27 @@ from .api import WebsitesVS, PagesVS, ProductsVS, SimilarProductsVS
 # Update an existing books: PUT api/pages/{id}/
 # Remove a book: DELETE api/pages/{id}/
 
+# get websites by user id: POST api/websites/ID/
+# {
+#     "user_id":1
+# }
+# get pages by website id: POST api/pages/ID/
+# {
+#     "website_id":1
+# }
+# get products by website id: POST api/products/ID/
+# {
+#     "website_id":1
+# }
+# get similar products by product id: POST api/similar/ID/
+# {
+#     "product_id":1
+# }
+
 router = routers.DefaultRouter()
-router.register('api/websites', WebsitesVS, 'websites') 
+router.register('api/websites', WebsitesVS, 'websites')
 router.register('api/pages', PagesVS, 'pages')
 router.register('api/products', ProductsVS, 'products')
-router.register('api/similar', SimilarProductsVS, 'similar') 
+router.register('api/similar', SimilarProductsVS, 'similar')
 
 urlpatterns = router.urls

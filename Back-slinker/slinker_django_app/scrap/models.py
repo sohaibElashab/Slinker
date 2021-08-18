a428e92c66086ee0c4b28286a4b49ad59ab2c48f
AyoubTougui <mr.tougui.ayoub@gmail.com>
2021-08-18 15:36:22 +0100

    .

--- Back-slinker/slinker_django_app/scrap/models.py
+++ Back-slinker/slinker_django_app/scrap/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 
 class Websites(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     user_id = models.IntegerField(null=False)
     page_number = models.IntegerField(null=False)

659055b41a7daf7c7d882d36114d3e0dc5c15a48
sohaibElashab <sohaibelashab06@gmail.com>
2021-08-20 18:16:23 +0100

    .

--- Front-slinker/slinker-react-app/src/Components/Layouts/SiteResultes.js
+++ Front-slinker/slinker-react-app/src/Components/Layouts/SiteResultes.js
@@ -1,46 +1,67 @@
 import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-const SiteResultes = ({ show }) => {
+const SiteResultes = ({ id , name , logo }) => {
+  const products = useSelector((state) => state.products.products);
+  var result = []
+  var Affilliate = []
+  var countUNVB = 0
+  var countVB = 0
+  var countER = 0
+  var countLinks = null
+  if(products !== undefined){
+      result = products.filter(product => product.website_id == id);
+      countLinks = products.filter(product => product.website_id == id);
+  } 
+  if(result !== []){
+    result.map((el) => {
+      Affilliate.push(el.affiliat_id)
+      el.status == 'UNAVAILABLE' ? ++countUNVB : el.status == 'AVAILABLE' ? ++countVB : ++countER
+    })
+    Affilliate = [...new Set(Affilliate)]
+
+  } 
   return (
     <div className="site__sites">
       <div className="site__title__sites">
         <div className="site__title__logo__sites">
           <img
             className=""
-            src="https://localhost:3000/Assets/eysi-full.png"
+            src={logo}
             alt=""
           />
+          {name}
         </div>
         <div className="site__title__edit__sites">
           <i className="fas fa-cog"></i>
-          <i className="fas fa-trash"></i>
+          <i className="fas fa-trash"></i> 
         </div>
       </div>
       <hr />
       <div className="site__resultes__sites">
         <div className="site__resulte__sites">
           <div className="site__resulte__title__sites">Link Checked</div>
-          <div className="blue__sites">112</div>
+          <div className="blue__sites">{countLinks !== null && countLinks.length}</div>
         </div>
         <div className="site__resulte__sites">
           <div className="site__resulte__title__sites">Available</div>
-          <div className="green__sites">86</div>
+          <div className="green__sites">{countVB}</div>
         </div>
         <div className="site__resulte__sites">
           <div className="site__resulte__title__sites">Unavailable</div>
-          <div className="red__sites">12</div>
+          <div className="red__sites">{countUNVB}</div>
         </div>
         <div className="site__resulte__sites">
           <div className="site__resulte__title__sites">Error 404</div>
-          <div className="gris__sites">14</div>
+          <div className="gris__sites">{countER}</div>
         </div>
         <hr />
         <div className="site__resulte__sites">
           <div className="site__resulte__title__sites">
             Affiliate id checked
           </div>
-          <div className="blue__sites">2</div>
+          <div className="blue__sites">{Affilliate !== null && Affilliate.length}</div>
         </div>
         <hr />
         <div className="site__resulte__sites">
--- Front-slinker/slinker-react-app/src/Components/Pages/Sites.js
+++ Front-slinker/slinker-react-app/src/Components/Pages/Sites.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import NavSearch from '../Layouts/NavSearch'
 import Sidebar from '../Layouts/Sidebar'
 import SiteResultes from '../Layouts/SiteResultes'
 
 const Sites = () => {
+    const websites = useSelector((state) => state.websites.websites);
     return (
     <div className="content">
         <Sidebar/>
@@ -15,12 +17,13 @@ const Sites = () => {
                     <button>Add site +</button>
                 </div>
                 <div className="sites__sites">
-                    <SiteResultes/>
-                    <SiteResultes/>
-                    <SiteResultes/>
-                    <SiteResultes/>
-                    <SiteResultes/>
-                    <SiteResultes/>
+                    {
+                        websites !== undefined ? 
+                            websites.map((el) => (
+                                <SiteResultes id={el.id} name={el.name} logo={el.image}/>
+                            )) 
+                        : ""
+                    }
                 </div>
             </div>
         </main>

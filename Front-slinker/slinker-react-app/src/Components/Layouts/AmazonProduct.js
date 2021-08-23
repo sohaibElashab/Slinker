96fdd0df26cc8be1de8ec748fedabb95fd290f15
AyoubTougui <mr.tougui.ayoub@gmail.com>
2021-08-23 20:12:44 +0100

    .

--- Front-slinker/slinker-react-app/src/Components/Layouts/AmazonProduct.js
+++ Front-slinker/slinker-react-app/src/Components/Layouts/AmazonProduct.js
@@ -1,21 +1,40 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const AmazonProduct = ({ show , id }) => {
+const AmazonProduct = ({ show, id, GetSimilar }) => {
   const products = useSelector((state) => state.products.products);
-  var result = []
-  var Affilliate = []
-  if(products !== undefined){
-      result = products.filter(product => product.website_id == id);
-  } 
-  if(result !== []){
-    result.map((el) => {
-      Affilliate.push(el.affiliat_id)
-    })
-    Affilliate = [...new Set(Affilliate)]
-    console.log(Affilliate)
-  } 
+  const [list, setlist] = useState(products);
+  const [AFlist, setAFlist] = useState([]);
+  useEffect(() => {
+    if (products !== undefined) {
+      var Affilliate = [];
+      var result = [];
+
+      result = products.filter((product) => product.website_id == id);
+      result.map((el) => {
+        Affilliate.push(el.affiliat_id);
+      });
+      Affilliate = [...new Set(Affilliate)];
+      setlist(result);
+      setAFlist(Affilliate);
+    }
+  }, [products, id]);
+
+  console.log(list);
+  console.log(AFlist);
+
+  const testChange = (e) => {
+    console.log(e.target.value);
+    const result = products.filter((product) => product.website_id == id);
+    setlist(result);
+    if (e.target.value !== "All") {
+      const filtered = result.filter(
+        (product) => product.affiliat_id == e.target.value
+      );
+      setlist(filtered);
+    }
+  };
   return (
     <div className="table__amazon__products__ISC">
       <div className="amazon__products__header__ISC">
@@ -38,13 +57,15 @@ const AmazonProduct = ({ show , id }) => {
             }`}
           >
             <span className="blue__ISC">Affilliate Id:</span>
-            <select name="" id="">
-              <option value="">All</option>
-              {
-                Affilliate.map((el) => (
-                  <option value="" key={el}>{el}</option>
-                ))
-              }
+            <select name="" id="" onChange={testChange}>
+              <option value="All">All</option>
+              {AFlist !== undefined
+                ? AFlist.map((el) => (
+                    <option value={el} key={el}>
+                      {el}
+                    </option>
+                  ))
+                : ""}
             </select>
           </div>
           {show && (
@@ -72,30 +93,46 @@ const AmazonProduct = ({ show , id }) => {
         </div>
       </div>
       <div className="amazon__products__body__ISC">
-      {
-         result !== [] ? result.map((element) => (
-            <div className="amazon__product__ISC" key={element.id}>
-              {show && (
+        {list !== undefined
+          ? list.map((element) => (
+              <div
+                className="amazon__product__ISC"
+                key={element.id}
+                onClick={() => GetSimilar(element.id)}
+                style={{ cursor: "pointer" }}
+              >
+                {show && (
+                  <span>
+                    <i className="fas fa-chevron-down"></i>
+                  </span>
+                )}
                 <span>
-                  <i className="fas fa-chevron-down"></i>
+                  <img src={element.image} alt="" />
                 </span>
-              )}
-              <span>
-                <img src={element.image} alt="" />
-              </span>
-              <span className="text1__ISC">
-                {element.name}
-              </span>
-              <span className={element.status == 'UNAVAILABLE' ? 'red__bg__ISC text2__ISC' : element.status == 'AVAILABLE' ? 'green__bg__ISC text2__ISC' : 'gris__bg__ISC text2__ISC'}>{element.status}</span>
+                <span className="text1__ISC">{element.name}</span>
+                <span
+                  className={
+                    element.status == "UNAVAILABLE"
+                      ? "red__bg__ISC text2__ISC"
+                      : element.status == "AVAILABLE"
+                      ? "green__bg__ISC text2__ISC"
+                      : "gris__bg__ISC text2__ISC"
+                  }
+                >
+                  {element.status}
+                </span>
 
-              <span className={"text2__ISC"}>{element.status}</span>
-              <span>3.7 ({element.number_reviews} reviews)</span>
-              <span className="blue__bg__ISC text3__ISC">{element.affiliat_id}</span>
-              {show && <span className="text4__ISC">View Product Report</span>}
-            </div>
-          ))
-        :"loading ...!"
-      }
+                {/* <span className={"text2__ISC"}>{element.status}</span> */}
+                <span>3.7 ({element.number_reviews} reviews)</span>
+                <span className="blue__bg__ISC text3__ISC">
+                  {element.affiliat_id}
+                </span>
+                {show && (
+                  <span className="text4__ISC">View Product Report</span>
+                )}
+              </div>
+            ))
+          : "loading ...!"}
       </div>
     </div>
   );
--- Front-slinker/slinker-react-app/src/Components/Pages/Issues.js
+++ Front-slinker/slinker-react-app/src/Components/Pages/Issues.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavSearch from "../Layouts/NavSearch";
 import Sidebar from "../Layouts/Sidebar";
 import MustSubscribe from "../Layouts/MustSubscribe";
@@ -8,12 +8,15 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Issues = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const similar = useSelector((state) => state.similar.similars);
-  var result = []
-  if(similar !== undefined){
-      result = similar.filter(simi => simi.product_id == 19);
-  } 
+  const [list, setlist] = useState(undefined);
+
+  const GetSimilar = (product_id) => {
+    console.log(product_id);
+    const result = similar.filter((simi) => simi.product_id === product_id);
+    setlist(result);
+  };
   return (
     <div className="content">
       <Sidebar />
@@ -31,25 +34,23 @@ const Issues = () => {
             <div className="resultes__ISC">
               <div className="amazon__products__ISC ">
                 Amazon Products
-                <AmazonProduct id={id}  show={true} /> 
+                <AmazonProduct id={id} show={true} GetSimilar={GetSimilar} />
               </div>
               <div className="amazon__similar__products__Issues">
                 Amazon Similar Products
                 <div className="table__amazon__similar__products__Issues">
-                {
-                  result !== [] && result.map((element) => (
-                    <div className="amazon__similar__product__Issues">
-                      <img
-                        src={element.image}
-                        alt=""
-                      />
-                      <span>
-                        {element.name}
-                      </span>
-                      <button>View</button>
-                    </div>
-                  ))
-                }
+                  {list !== undefined
+                    ? list.map((element) => (
+                        <div
+                          className="amazon__similar__product__Issues"
+                          key={element.id}
+                        >
+                          <img src={element.image} alt="" />
+                          <span>{element.name}</span>
+                          <button>View</button>
+                        </div>
+                      ))
+                    : "Select a product"}
                 </div>
               </div>
             </div>

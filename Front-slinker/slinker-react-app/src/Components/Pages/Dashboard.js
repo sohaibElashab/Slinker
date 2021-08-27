c7ad3e3c79e9ed83fb8ea52715f05dcf5992ea45
sohaibElashab <sohaibelashab06@gmail.com>
2021-08-27 21:16:12 +0100

    .

--- Front-slinker/slinker-react-app/src/Components/Pages/Dashboard.js
+++ Front-slinker/slinker-react-app/src/Components/Pages/Dashboard.js
@@ -3,6 +3,8 @@ import Sidebar from '../Layouts/Sidebar'
 import ControlButton from '../Layouts/ControlButton'
 import { useSelector } from 'react-redux';
 import NavSearch from '../Layouts/NavSearch';
+import {Bar, defaults, Doughnut, Line} from 'react-chartjs-2'
+
 
 const Dashboard = () => {
   const websites = useSelector((state) => state.websites.websites);
@@ -43,21 +45,103 @@ const Dashboard = () => {
             <div className="health__products__dashboard">
               <div className="health__dashboard">
                 Site Health Score
-                <div className="health__diagram__dashboard"></div>
+                <div className="health__diagram__dashboard">
+                  <Bar 
+                        data={{
+                            labels: ['Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'Mai' , 'Jun' , 'Jul'],
+                            datasets: [
+                                {
+                                    label: '# of Votes',
+                                    data: [12, 19, 3, 5, 2, 3 , 7 , 16],
+                                    backgroundColor: [
+                                        'rgba(80, 62, 157, 1)',
+                                    ],
+                                    borderWidth: 1
+                                }
+                            ]
+                        }}
+                        height={200}
+                        width={100}
+                        options={{ maintainAspectRatio: false }}
+                    />
+                </div>
               </div>
               <div className="products__dashboard none">
                 Total Products
-                <div className="products__diagram__dashboard"></div>
+                <div className="products__diagram__dashboard">
+                  <Doughnut 
+                        data={{
+                            labels: ['Available', 'Unavailable', 'Error 404'],
+                            datasets: [
+                                {
+                                    label: '# of Votes',
+                                    data: [12, 19, 3],
+                                    backgroundColor: [
+                                        '#503E9D',
+                                        '#C5ADD2',
+                                        '#DBDCEA',
+                                    ],
+                                    borderWidth: 1
+                                }
+                            ]
+                        }}
+                        height={200}
+                        width={100}
+                        options={{ maintainAspectRatio: false }}
+                    />
+
+                </div>
               </div>
             </div>
             <div className="pages__links__dashboard none">
               <div className="pages__dashboard">
                 Total Page
-                <div className="page__dashboard"></div>
+                <div className="page__dashboard">
+                  <Line 
+                        data={{
+                            labels: ['Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'Mai' , 'Jun' , 'Jul'],
+                            datasets: [
+                                {
+                                    label: '# of Votes',
+                                    data: [12, 19, 3, 5, 2, 3 , 7 , 16],
+                                    backgroundColor: [
+                                        'rgba(80, 62, 157, 1)',
+                                    ],
+                                    borderWidth: 1
+                                }
+                            ]
+                        }}
+                        height={200}
+                        width={100}
+                        options={{ maintainAspectRatio: false }}
+                    />
+                </div>
               </div>
               <div className="links__dashboard">
                 Total Links
-                <div className="link__dashboard"></div>
+                <div className="link__dashboard">
+                  <Doughnut 
+                        data={{
+                            labels: ['Available', 'Unavailable', 'Error 404'],
+                            datasets: [
+                                {
+                                    label: '# of Votes',
+                                    data: [12, 19, 3],
+                                    backgroundColor: [
+                                        '#503E9D',
+                                        '#C5ADD2',
+                                        '#DBDCEA',
+                                    ],
+                                    borderWidth: 1
+                                }
+                            ]
+                        }}
+                        height={200}
+                        width={100}
+                        options={{ maintainAspectRatio: false }}
+                    />
+
+                </div>
               </div>
               <ControlButton big={false}/>
             </div>
